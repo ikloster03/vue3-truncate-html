6b4834ef23cfa668e76191598c2b336174131629
Ivan Monastyrev <ikloster@yandex.ru>
2022-02-22 17:12:02 +0300

    docs: update example

--- docs/VueTruncateHtmlExample.vue
+++ docs/VueTruncateHtmlExample.vue
@@ -33,42 +33,22 @@ export default {
       facere hic id impedit labore laboriosam magni molestiae nemo non numquam officiis porro,
       quibusdam tempora totam vel voluptate voluptatem voluptatum. Ad adipisci architecto,
       beatae blanditiis corporis cumque dolor, eaque excepturi exercitationem magnam nihil optio perferendis perspiciatis qui quis,
-      quo rem rerum ullam unde veniam. Dicta distinctio excepturi hic veniam? Cumque earum laboriosam laudantium praesentium ut.
-      Accusamus ad architecto blanditiis, consequuntur culpa ducimus eligendi eos eum eveniet facilis nam obcaecati odit quas sint ut,
-      vitae voluptatibus? Alias amet animi at atque aut distinctio earum error est,
-      exercitationem explicabo fugit impedit iste iure iusto nesciunt nisi,
-      obcaecati perspiciatis quasi qui quisquam reprehenderit sapiente sequi suscipit velit veniam.
-      Debitis delectus ea eius esse ex expedita nihil officia placeat repellendus voluptas?
-      Atque aut dolore error, ipsum itaque minima nisi quo repellat repellendus, saepe sunt,
-      veritatis vero? Ab accusamus ducimus, ex exercitationem id laborum, maxime minus nobis odio,
-      placeat possimus reiciendis sed ullam. Assumenda blanditiis cum debitis deserunt est harum iure laborum,
-      modi mollitia nemo praesentium vitae voluptate?
-      Ad architecto consequuntur dicta dolorum eos inventore maiores necessitatibus neque nihil nulla officia officiis velit, veritatis.
-      Asperiores, sapiente voluptas!`;
+      `;
 
     const html = `
-      <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</b>
-      <ul>
-        <li>
-            <a href="https://google.com">Google.com</a>
-            Adipisci animi autem beatae consequuntur debitis delectus deleniti ducimus enim,
-        </li>
-        <li>facere hic id impedit labore laboriosam magni molestiae nemo non numquam officiis porro,</li>
-        <li>quibusdam tempora totam vel voluptate voluptatem voluptatum. Ad adipisci architecto,</li>
-      </ul>
-      <i>beatae blanditiis corporis cumque dolor</i>, eaque excepturi exercitationem magnam nihil optio perferendis perspiciatis qui quis,
-      quo rem rerum ullam unde veniam. Dicta distinctio excepturi hic veniam? Cumque earum laboriosam laudantium praesentium ut.
-      Accusamus ad architecto blanditiis, consequuntur culpa ducimus eligendi eos eum eveniet facilis nam obcaecati odit quas sint ut,
-      vitae voluptatibus? Alias amet animi at atque aut distinctio earum error est,
-      exercitationem explicabo fugit impedit iste iure iusto nesciunt nisi,
-      obcaecati perspiciatis quasi qui quisquam reprehenderit sapiente sequi suscipit velit veniam.
-      Debitis delectus ea eius esse ex expedita nihil officia placeat repellendus voluptas?
-      Atque aut dolore error, ipsum itaque minima nisi quo repellat repellendus, saepe sunt,
-      veritatis vero? Ab accusamus ducimus, ex exercitationem id laborum, maxime minus nobis odio,
-      placeat possimus reiciendis sed ullam. Assumenda blanditiis cum debitis deserunt est harum iure laborum,
-      modi mollitia nemo praesentium vitae voluptate?
-      Ad architecto consequuntur dicta dolorum eos inventore maiores necessitatibus neque nihil nulla officia officiis velit, veritatis.
-      Asperiores, sapiente voluptas!`;
+      <p>
+        <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</b>
+        <ul>
+          <li>
+              <a href="https://google.com">Google.com</a>
+              Adipisci animi autem beatae consequuntur debitis delectus deleniti ducimus enim,
+          </li>
+          <li>facere hic id impedit labore laboriosam magni molestiae nemo non numquam officiis porro,</li>
+          <li>quibusdam tempora totam vel voluptate voluptatem voluptatum. Ad adipisci architecto,</li>
+        </ul>
+        <i>beatae blanditiis corporis cumque dolor</i>, eaque excepturi exercitationem magnam nihil optio perferendis perspiciatis qui quis,
+      </p>
+     `;
 
     return {
       isTruncated, isTruncated2, text, html,
