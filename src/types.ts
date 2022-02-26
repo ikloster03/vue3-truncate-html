c0039b11529d41264e9f2d8c828f1fceb4b9bb79
Ivan Monastyrev <ikloster@yandex.ru>
2022-02-26 23:53:03 +0300

    library: add types

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export type Type = 'text' | 'html';
+
+export type Buttons = {
+  more: string
+  less: string
+}
+
+export type Classes = {
+  container: string
+  content: string
+  contentHtml: string
+  contentText: string
+  button: string
+  buttonMore: string
+  buttonLess: string
+}
