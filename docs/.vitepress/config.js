6704186ffc43c84ac29d66ce356b1ad8deb5025c
Ivan Monastyrev <ikloster@yandex.ru>
2022-02-27 21:06:12 +0300

    docs: update config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,31 +4,73 @@ export default defineConfig({
   lang: 'en-US',
   title: 'vue3-truncate-html',
   description: 'A Vue 3 component for html truncating',
+  locales: {
+    '/en/': {
+      lang: 'en-US', // this will be set as the lang attribute on <html>
+      title: 'vue3-truncate-html',
+      description: ''
+    },
+    '/ru/': {
+      lang: 'ru-RU', // this will be set as the lang attribute on <html>
+      title: 'vue3-truncate-html',
+      description: ''
+    },
+  },
   themeConfig: {
     docsDir: 'docs',
+    locales: {
+      '/en/': {
+        label: 'English',
+        selectText: 'English',
+      },
+      '/ru/': {
+        label: 'Русский',
+        selectText: 'Русский',
+      },
+    },
     nav: [
       {
         text: 'Github',
         link: 'https://github.com/ikloster03/vue3-truncate-html',
       },
     ],
-    sidebar: [
-      {
-        text: 'Introduction',
-        link: '/',
-      },
-      {
-        text: 'Getting started',
-        link: '/getting-started',
-      },
-      {
-        text: 'API',
-        link: '/api',
-      },
-      // {
-      //   text: 'Examples',
-      //   link: '/examples',
-      // },
-    ],
+    sidebar: {
+      '/en/': [
+        {
+          text: 'Introduction',
+          link: '/en/index',
+        },
+        {
+          text: 'Getting started',
+          link: '/en/getting-started',
+        },
+        {
+          text: 'API',
+          link: '/en/api',
+        },
+        // {
+        //   text: 'Examples',
+        //   link: '/examples',
+        // },
+      ],
+      '/ru/': [
+        {
+          text: 'Вступление',
+          link: '/ru/index',
+        },
+        {
+          text: 'Быстрый старт',
+          link: '/ru/getting-started',
+        },
+        {
+          text: 'API',
+          link: '/ru/api',
+        },
+        // {
+        //   text: 'Examples',
+        //   link: '/examples',
+        // },
+      ]
+    }
   }
 })
