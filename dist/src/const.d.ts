354a9fa3d6f9c31afe67751b13610633ffffd7eb
Ivan Monastyrev <ikloster@yandex.ru>
2025-07-08 00:30:35 +0300

    chore: remove unused files and update .gitignore

--- dist/src/const.d.ts
+++
@@ -1,4 +0,0 @@
-import { Buttons, Classes } from './types';
-export declare const TEXT: string, HTML: string;
-export declare const defaultClasses: Classes;
-export declare const defaultButtons: Buttons;
