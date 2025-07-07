8d61df2fbc0762b4ffb6e90a4512ea5136ebe953
Ivan Monastyrev <ikloster@yandex.ru>
2025-07-08 00:10:49 +0300

    feat: add commitlint configuration and simple-git-hooks

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'perf',
+        'ci',
+        'build',
+        'revert',
+      ],
+    ],
+  },
+};
